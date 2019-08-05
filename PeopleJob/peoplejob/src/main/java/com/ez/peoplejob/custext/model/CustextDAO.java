3d4b52c1d1f10bf604048b00c91cc71517c1aff8
ezen-test <ezen-test@ezen-test-PC>
2019-08-05 17:19:04 +0900

    0805 남준열 커밋

---
+++ PeopleJob/peoplejob/src/main/java/com/ez/peoplejob/custext/model/CustextDAO.java
@@ -0,0 +1,13 @@
+package com.ez.peoplejob.custext.model;
+
+import java.util.List;
+import java.util.Map;
+
+public interface CustextDAO {
+	int insertCustext(CustextVO vo);
+	int ReinsertCustext(CustextVO vo);
+	List<CustextVO> mycustext(Map<String,Object> map);
+	CustextVO CustextView(int custextCode);
+	int Editcustext(CustextVO vo);
+	int Delcustext(int custextCode);
+}
