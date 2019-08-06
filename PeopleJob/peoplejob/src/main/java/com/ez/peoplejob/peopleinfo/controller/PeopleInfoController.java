31886985842fb5423194ced7624cdf7ea7ff33db
ezen-test <ezen-test@ezen-test-PC>
2019-08-06 18:06:05 +0900

    남준열 0806충돌해결

--- PeopleJob/peoplejob/src/main/java/com/ez/peoplejob/peopleinfo/controller/PeopleInfoController.java
+++ PeopleJob/peoplejob/src/main/java/com/ez/peoplejob/peopleinfo/controller/PeopleInfoController.java
@@ -28,145 +28,110 @@ import com.ez.peoplejob.resume.model.ResumeVO;
 @RequestMapping("/peopleinfo")
 public class PeopleInfoController {
 
-	private Logger logger=LoggerFactory.getLogger(PeopleInfoController.class);
-	@Autowired
-	private ResumeService resumeService;
-	@Autowired
-	private PeopleInfoService peopleinfoService;
-	
-	@RequestMapping("/peopleinfolist.do")
-	public String peopleinfolist(
-		 HttpSession session,
-			@ModelAttribute SearchVO searchVo,Model model,@RequestParam(required = false) String[] term,@RequestParam(required = false) String[] age,
-			@RequestParam(required = false) String[] graduatetype,@RequestParam(required = false) String[] sido,@RequestParam(required = false) String[] btypename1 ,
-			@RequestParam(required = false) String[] btypename2,@RequestParam(required = false) String[] btypename3,@RequestParam(required = false) String[] firstname,
-			@RequestParam(required = false) String[] secondname,@RequestParam(required = false) String[] thirdname,@RequestParam(value="resumeCode", defaultValue = "0") int resumeCode) {
-		String id=(String)session.getAttribute("memberid");
-		if(id==null) {
-			id="비회원";
-		}
-		ResumeVO vo=resumeService.selectByMemverid(id);
-		logger.info("로그인 vo={}",vo);
-		logger.info("인재정보 리스트");
-		//1]PaginationInfo 객체 생성
-		PaginationInfo pagingInfo=new PaginationInfo();
-		pagingInfo.setBlockSize(WebUtility.BLOCK_SIZE);
-		pagingInfo.setRecordCountPerPage(WebUtility.RECORD_COUNT_PER_PAGE);
-		pagingInfo.setCurrentPage(searchVo.getCurrentPage());
-		
-		//2]SearchVo에 페이징 관련 변수 세팅
-		searchVo.setRecordCountPerPage(WebUtility.RECORD_COUNT_PER_PAGE);
-		searchVo.setFirstRecordIndex(pagingInfo.getFirstRecordIndex());
-		logger.info("셋팅 후 serchVo={}",searchVo);
-		
-		List<ResumeVO> list=new ArrayList<ResumeVO>();
-		Map<String, Object> map= new HashMap<String, Object>();
-		logger.info("term={}",term);
-		logger.info("age={}",age);
-		logger.info("graduatetype={}",graduatetype);
-		logger.info("sido={}",sido);
-		logger.info("btypename1={}",btypename1);
-		logger.info("btypename2={}",btypename2);
-		logger.info("btypename3={}",btypename3);
-		logger.info("firstname={}",firstname);
-		logger.info("secondname={}",secondname);
-		logger.info("thirdname={}",thirdname);
-		map.put("term",term);
-		map.put("age",age);
-		map.put("graduatetype",graduatetype);
-		map.put("sido",sido);
-		map.put("btypename1",btypename1);
-		map.put("btypename2",btypename2);
-		map.put("btypename3",btypename3);
-		map.put("firstname",firstname);
-		map.put("secondname",secondname);
-		map.put("thirdname",thirdname);
-		map.put("firstRecordIndex", searchVo.getFirstRecordIndex());
-		map.put("recordCountPerPage", searchVo.getRecordCountPerPage());
-		map.put("resumeCode", resumeCode);
-		logger.info("map={}",map);
-		list=peopleinfoService.selectPeoplew(map);
-		
-		logger.debug("조회결과 list.size()={}",list.size());
-		List<ResumeVO> resumelist=new ArrayList<ResumeVO>();
-		logger.info("list.size={} , resumelist.size={}",list.size(),resumelist.size());
-		if(resumelist.size()>0) {
-			for(int i=0;i<list.size();i++){
-			resumelist.add(resumeService.selectResumeByNo(list.get(i).getResumeCode()));
-				logger.info("resumelist[{}]={}",i,resumelist.get(i).getResumeCode());
-			}
-		}
-		logger.info("resumelist.size={}",resumelist.size());
-		int totalRecord=0;
-		
-		/* totalRecord=peopleinfoService.selectTotalCountPeople(map); */
-		 
-		 logger.info("전체 레코드 개수 조회 결과, totalRecord={}",totalRecord);
-		
-		
-		//5]PaginationInfo에 totalRecord값셋팅
-		pagingInfo.setTotalRecord(totalRecord);
-		//3
-		model.addAttribute("pagingInfo", pagingInfo);
-		model.addAttribute("list",list);
-		model.addAttribute("resumelist",resumelist);
-		model.addAttribute("vo", vo);
-		return "peopleinfo/peopleinfolist";
-		
-		
-		
-	}
-	@RequestMapping("/peopleinfodetail.do")
-	public String peopleinfodetail(@RequestParam(required = false) int resumeCode,@RequestParam(required = false) String[] term,@ModelAttribute ResumeVO resumeVo,HttpSession session,Model model) {
-		logger.info("resumeCode={}",resumeCode);
-		ResumeVO vo=resumeService.selectResumeByNo(resumeCode);
-		String id=(String)session.getAttribute("memberid");
-		if(id==null) {
-			id="비회원";
-		}
-		List<ResumeVO> list=new ArrayList<ResumeVO>();
-		Map<String, Object> map = new HashMap<String, Object>();
-		map.put("term", term);
-		ResumeVO vo1=resumeService.selectByMemverid(id);
-		ResumeVO vo2=resumeService.selectBydesiredWorkCode(vo.getDesiredWorkCode());
-		ResumeVO vo3=resumeService.selectByacademicCode(vo.getAcademicCode());
-		ResumeVO vo4=resumeService.selectBydvCode(vo.getDvCode());
-		ResumeVO vo5=resumeService.selectBylanglicenceCode(vo.getLanglicenceCode());
-		ResumeVO vo6=resumeService.selectBylicenceCode(vo.getLicenceCode());
-		ResumeVO vo7=resumeService.selectBylocation(vo.getLocalCode());
-		ResumeVO vo8=resumeService.selectBylocation2(vo.getLocalCode2());
-		ResumeVO vo9=resumeService.selectBybtype1(vo.getBtypeCode1());
-		ResumeVO vo10=resumeService.selectBybtype2(vo.getBtypeCode2());
-		ResumeVO vo11=resumeService.selectBybtype3(vo.getBtypeCode3());
-		ResumeVO vo12=resumeService.selectByfirst(vo.getFirstCode());
-		ResumeVO vo13=resumeService.selectBysecond(vo.getSecondCode());
-		ResumeVO vo14=resumeService.selectBythird(vo.getThirdCode());
-		List<ResumeVO> vo15=peopleinfoService.selectCareer(map);
-		logger.info("상세보기 결과 vo={}", vo);
-		
-		model.addAttribute("vo", vo);
-		model.addAttribute("vo1", vo1);
-		model.addAttribute("vo2", vo2);
-		model.addAttribute("vo3", vo3);
-		model.addAttribute("vo4", vo4);
-		model.addAttribute("vo5", vo5);
-		model.addAttribute("vo6", vo6);
-		model.addAttribute("vo7", vo7);
-		model.addAttribute("vo8", vo8);
-		model.addAttribute("vo9", vo9);
-		model.addAttribute("vo10", vo10);
-		model.addAttribute("vo11", vo11);
-		model.addAttribute("vo12", vo12);
-		model.addAttribute("vo13", vo13);
-		model.addAttribute("vo14", vo14);
-		model.addAttribute("vo15", vo15);
-	
-		return "peopleinfo/peopleinfodetail";
-	}
-	@RequestMapping("/peopleinfowhere.do")
-	public String peopleinfowhere() {
-		logger.info("조건지정");
-		return "peopleinfo/peopleinfowhere";
-	}		
+   private Logger logger=LoggerFactory.getLogger(PeopleInfoController.class);
+   @Autowired
+   private ResumeService resumeService;
+   @Autowired
+   private PeopleInfoService peopleinfoService;
+   
+   @RequestMapping("/peopleinfolist.do")
+   public String peopleinfolist(
+       HttpSession session,
+         @ModelAttribute SearchVO searchVo,Model model,@RequestParam(required = false) String[] term,@RequestParam(required = false) String[] age,
+         @RequestParam(required = false) String[] graduatetype,@RequestParam(required = false) String[] sido,@RequestParam(required = false) String[] gugun,@RequestParam(required = false) String[] btypename1 ,
+         @RequestParam(required = false) String[] btypename2,@RequestParam(required = false) String[] btypename3,@RequestParam(required = false) String[] firstname,
+         @RequestParam(required = false) String[] secondname,@RequestParam(required = false) String[] thirdname,@RequestParam(value="resumeCode", defaultValue = "0") int resumeCode) {
+      String id=(String)session.getAttribute("memberid");
+      if(id==null) {
+         id="비회원";
+      }
+      ResumeVO vo1=resumeService.selectByMemverid(id);
+      List<ResumeVO> vo=peopleinfoService.selectResumeView(resumeCode);
+      logger.info("로그인 vo={}",vo1);
+      logger.info("인재정보 리스트");
+      //1]PaginationInfo 객체 생성
+      PaginationInfo pagingInfo=new PaginationInfo();
+      pagingInfo.setBlockSize(WebUtility.BLOCK_SIZE);
+      pagingInfo.setRecordCountPerPage(WebUtility.RECORD_COUNT_PER_PAGE);
+      pagingInfo.setCurrentPage(searchVo.getCurrentPage());
+      
+      //2]SearchVo에 페이징 관련 변수 세팅
+      searchVo.setRecordCountPerPage(WebUtility.RECORD_COUNT_PER_PAGE);
+      searchVo.setFirstRecordIndex(pagingInfo.getFirstRecordIndex());
+      logger.info("셋팅 후 serchVo={}",searchVo);
+      
+      List<ResumeVO> list=new ArrayList<ResumeVO>();
+      Map<String, Object> map= new HashMap<String, Object>();
+      logger.info("term={}",term);
+      logger.info("age={}",age);
+      logger.info("graduatetype={}",graduatetype);
+      logger.info("sido={}",sido);
+      logger.info("gugun={}",gugun);
+      logger.info("btypename1={}",btypename1);
+      logger.info("btypename2={}",btypename2);
+      logger.info("btypename3={}",btypename3);
+      logger.info("firstname={}",firstname);
+      logger.info("secondname={}",secondname);
+      logger.info("thirdname={}",thirdname);
+      map.put("term",term);
+      map.put("age",age);
+      map.put("graduatetype",graduatetype);
+      map.put("sido",sido);
+      map.put("gugun",gugun);
+      map.put("btypename1",btypename1);
+      map.put("btypename2",btypename2);
+      map.put("btypename3",btypename3);
+      map.put("firstname",firstname);
+      map.put("secondname",secondname);
+      map.put("thirdname",thirdname);
+      map.put("firstRecordIndex", searchVo.getFirstRecordIndex());
+      map.put("recordCountPerPage", searchVo.getRecordCountPerPage());
+      map.put("resumeCode", resumeCode);
+      logger.info("map={}",map);
+      list=peopleinfoService.selectPeoplew(map);
+      
+      logger.debug("조회결과 list.size()={}",list.size());
+      
+      int totalRecord=0;
+      
+      totalRecord=peopleinfoService.selectTotalCountPeople(map);
+       
+       logger.info("전체 레코드 개수 조회 결과, totalRecord={}",totalRecord);
+      
+      
+      //5]PaginationInfo에 totalRecord값셋팅
+      pagingInfo.setTotalRecord(totalRecord);
+      //3
+      model.addAttribute("pagingInfo", pagingInfo);
+      model.addAttribute("list",list);
+      model.addAttribute("vo1", vo1);
+      model.addAttribute("vo", vo);
+      return "peopleinfo/peopleinfolist";
+      
+      
+      
+   }
+   @RequestMapping("/peopleinfodetail.do")
+   public String peopleinfodetail(@RequestParam(defaultValue = "0") int resumeCode,@RequestParam(required = false) String[] term,@ModelAttribute ResumeVO resumeVo,HttpSession session,Model model) {
+      logger.info("resumeCode={}",resumeCode);
+      List<ResumeVO> vo=peopleinfoService.selectResumeView(resumeCode);
+      String id=(String)session.getAttribute("memberid");
+      if(id==null) {
+         id="비회원";
+      }
+      ResumeVO vo1=resumeService.selectByMemverid(id);
+      
+      
+      
+      model.addAttribute("회원 정보 vo1={}", vo1);
+      model.addAttribute("전체 vo={}", vo);
+   
+   
+      return "peopleinfo/peopleinfodetail";
+   }
+   @RequestMapping("/peopleinfowhere.do")
+   public String peopleinfowhere() {
+      logger.info("조건지정");
+      return "peopleinfo/peopleinfowhere";
+   }      
 }
-
--- PeopleJob/peoplejob/src/main/webapp/WEB-INF/views/peopleinfo/peopleinfolist.jsp
+++ PeopleJob/peoplejob/src/main/webapp/WEB-INF/views/peopleinfo/peopleinfolist.jsp
@@ -3,77 +3,86 @@
 <%@include file="../main/inc/top.jsp" %>
 <style>
 .divForm {
-    width: 1000px;
+    width: 1500px;
     margin: 0 auto;
     background: white;
     padding: 13px;
+    height: 900px;
 }
 #peopleinfo {
     background: #f2f4f7;
+        height: 871px;
 }
-
+h2 {
+    font-size: 18px;
+    margin: 0 0 16px;
+    margin-left: 286px;
+    font-weight: bold;
+}
 </style>
+<script type="text/javascript">
+
+</script>
 <div id="peopleinfo">
 <div class="divForm">
   <c:if test="${!empty param.term||!empty param.age||!empty param.graduatetype||!empty param.sido||!empty param.gugun
   ||!empty param.btypename1||!empty param.btypename2||!empty param.btypename3||!empty param.firstname||!empty param.secondname||!empty param.thirdname}">
-			<p>
-				검색결과 : ${pagingInfo.totalRecord}건 검색되었습니다.
-			</p>
-		</c:if>
-	<form name="frmSearch" method="post" style="border: 1px solid green;"
-   		action='<c:url value="/peopleinfo/peopleinfolist.do"/>'>
-   		<!-- 현재 페이지 hidden에 넣기 -->  
-   		<input type="hidden" name='currentPage' value="1" >
-   		<div id="whereall" style="margin-left: -209px;
+         <p>
+            검색결과 : ${pagingInfo.totalRecord}건 검색되었습니다.
+         </p>
+      </c:if>
+   <form name="frmSearch" method="post" style="border: 1px solid green;"
+         action='<c:url value="/peopleinfo/peopleinfolist.do"/>'>
+         <!-- 현재 페이지 hidden에 넣기 -->  
+         <input type="hidden" name='currentPage' value="1" >
+         <div id="whereall" style="margin-left: -209px;
     padding: 23px;">
-   		<div id="where" class="where">
-			<c:import url="peopleinfowhere.jsp"/>
-		</div> 
-		
-        <c:import url="../resume/occupation.jsp"/>
-		</div>
-        <c:if test="${empty list }" >	 
-        <div style="margin: 0 auto; min-height: 630px;position: relative;bottom: 0;width: 446px;">
-	 	<table class="table table-hover"style="position: relative;
+         <div id="where" class="where">
+         <c:import url="peopleinfowhere.jsp"/>
+      </div> 
+      
+      </div>
+        <c:if test="${empty list }" >    
+        
+       <table class="table table-hover"style="position: relative;
     bottom: -59px;">
-	 	<tr>
-	 		<td colspan="5" class="align_center">데이터가 존재하지 않습니다.</td>
-	 	</tr>
-	 	</table>
-		</div>
-		</c:if>
+       <tr>
+          <td colspan="5" class="align_center">데이터가 존재하지 않습니다.</td>
+       </tr>
+       </table>
+      
+      </c:if>
             <div class="divPage">
-	<!-- 이전블럭으로 이동하기 -->
-	<c:if test="${pagingInfo.firstPage>1 }">	
-		<a href="#" onclick="pageFunc(${pagingInfo.firstPage-1})">
-			<img src="<c:url value='/resources/images/first.JPG'/>" alt="이전블럭으로 이동">
-		</a>	
-	</c:if>
-	<!-- 페이지 번호 추가 -->
-	<!-- [1][2][3][4][5][6][7][8][9][10] -->
-	<c:forEach var="i" begin="${pagingInfo.firstPage }" end="${pagingInfo.lastPage }">
-		<c:if test="${i==pagingInfo.currentPage }">
-			<span style="color:blue;font-size: 1em">${i }</span>
-		</c:if>
-		<c:if test="${i!=pagingInfo.currentPage }">
-			<a href="#" onclick="pageFunc(${i})">
-				[${i}]</a>
-		</c:if>
-	</c:forEach>
-	<!--  페이지 번호 끝 -->
-	
-	<!-- 다음 블럭으로 이동하기 -->
-	<c:if test="${pagingInfo.lastPage<pagingInfo.totalPage }">	
-		<a href="#" onclick="pageFunc(${pagingInfo.lastPage+1})">
-			<img src="<c:url value='/resources/images/last.JPG'/>" alt="다음블럭으로 이동">
-		</a>
-	</c:if>
+   <!-- 이전블럭으로 이동하기 -->
+   <c:if test="${pagingInfo.firstPage>1 }">   
+      <a href="#" onclick="pageFunc(${pagingInfo.firstPage-1})">
+         <img src="<c:url value='/resources/images/first.JPG'/>" alt="이전블럭으로 이동">
+      </a>   
+   </c:if>
+   <!-- 페이지 번호 추가 -->
+   <!-- [1][2][3][4][5][6][7][8][9][10] -->
+   <c:forEach var="i" begin="${pagingInfo.firstPage }" end="${pagingInfo.lastPage }">
+      <c:if test="${i==pagingInfo.currentPage }">
+         <span style="color:blue;font-size: 1em">${i }</span>
+      </c:if>
+      <c:if test="${i!=pagingInfo.currentPage }">
+         <a href="#" onclick="pageFunc(${i})">
+            [${i}]</a>
+      </c:if>
+   </c:forEach>
+   <!--  페이지 번호 끝 -->
+   
+   <!-- 다음 블럭으로 이동하기 -->
+   <c:if test="${pagingInfo.lastPage<pagingInfo.totalPage }">   
+      <a href="#" onclick="pageFunc(${pagingInfo.lastPage+1})">
+         <img src="<c:url value='/resources/images/last.JPG'/>" alt="다음블럭으로 이동">
+      </a>
+   </c:if>
 </div>
-   		</form>
-   		 <c:if test="${!empty list }">
-   		<div class="talent_list" style="margin: 0 auto; min-height: 630px;position: relative;bottom: 0;width: 446px;">
-				    
+         </form>
+          <c:if test="${!empty list }">
+         <div class="talent_list" style="margin: 0 auto; min-height: 630px;position: relative;bottom: 0;width: 446px;">
+                
     <table class="table table-hover" style="position: relative;
     bottom: -59px;">
         <caption>인재검색 리스트</caption>
@@ -89,8 +98,8 @@
             <th scope="col" class="name" >이름</th>
             <th scope="col" class="contents">이력서 요약</th>
             <th scope="col" class="update" id="date_type">등록일</th>
-            <td id="peoplelist" >${vo1.membername}<br>(${vo1.membergender}|${vo15.age})</td>
-            <td id="peoplelist" >경력${vo15.term}&nbsp;${vo.resumeTitle}<br>
+            <td id="peoplelist" >${vo1.membername}<br>(${vo1.membergender}|${vo.age})</td>
+            <td id="peoplelist" >경력${vo.term}&nbsp;${vo.resumeTitle}<br>
             ${vo.schoolname}|${vo.hopepay}|${vo.sido}</td>
             <td id="peoplelist" >${vo.resumeRegdate}</td>
             
@@ -99,13 +108,7 @@
         </table>
         
         </div>
-        <div class="choice_wrap new mt20 mb10">
-  <div class="choice_inner">
-    <p class="total" id="total">
-      <strong>선택직종 :</strong> 
-    </p>
-  </div><!-- end choice_inner -->
-</div>
+       
         </c:if>
       </div> 
       </div> 
